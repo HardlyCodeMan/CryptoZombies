f5b2caa778b2fcb097b5bd74270cbbcca97788af
HardlyCodeMan <72480678+HardlyCodeMan@users.noreply.github.com>
2022-09-29 17:56:56 +0800

    Chapter 01 - Lesson 02

---
+++ Chapter01/Lesson02/ZombieFactory.sol
@@ -0,0 +1,60 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    /// @dev State Variables
+    uint dnaDigits = 16;
+    uint dnaModulus = 10**dnaDigits;
+
+    /// @dev Structs
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    /// @dev Arrays
+    Zombie[] public zombies;
+
+    /// @dev Mappings
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    /// @dev events
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    /// @dev functions
+
+    /***
+     * @param _name accepts string for Zombie name
+     * @param _dna accepts uint for Zombie dna
+     */
+    function _createZombie(string memory _name, uint _dna) internal {
+        /// @dev add new zombie to array
+        Zombie _newZombie = Zombie(_name, _dna);
+        uint id = zombies.push(_newZombie) -1;
+        // zombies.push(Zombie(_name, _dna)); // 1 liner variation of array push
+
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender] ++;
+
+        emit NewZombie(id, _name, _dna);
+    }
+
+    /***
+     * @param _str accepts string for entropy
+     */
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));    /// @audit insecure method of randomness
+        return rand % dnaModulus;
+    }
+
+    /***
+     * @dev only create a zombie if the account hasn't yet created one
+     * @param _name accepts string for Zombie name
+     */
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0, "Error creating zombie, user already created a zombie");
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
---
+++ Chapter01/Lesson02/ZombieFeeding.sol
@@ -0,0 +1,59 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./ZombieFactory.sol";
+
+/***
+ * @dev Interface to getKitty() from CryptoKittys project
+ */
+contract KittyInterface {
+    function getKitty(uint256 _id) external view returns (
+        bool isGestating,
+        bool isReady,
+        uint256 cooldownIndex,
+        uint256 nextActionAt,
+        uint256 siringWithId,
+        uint256 birthTime,
+        uint256 matronId,
+        uint256 sireId,
+        uint256 generation,
+        uint256 genes
+    );
+}
+
+contract ZombieFeeding is ZombieFactory {
+
+    /***
+     * @dev Load the CryptoKitty contract address
+     */
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
+    /***
+     * @dev Ensure msg.sender owns the zombie
+     * @param _zombieId accepts uint of an owned zombie
+     * @param _targetDna accepts uint of target zombie
+     */
+    function feedAndMultiply(uint _zombieId, uint _targetDna, _species) public {
+        require(msg.sender == zombieToOwner[_zombieId], "Error you don't own this zombie");
+        Zombie storage myZombie = zombies[_zombieId];
+        
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna) / 2;
+        
+        if(keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+            newDna = newDna - newDna % 100 + 99;
+        }
+        _createZombie("NoName", newDna);
+    }
+
+    /***
+     * @dev Get genes data from kittyContract for _kittyId
+     * @param _zombieId accepts uint for owned zombie checked in feedAndMultiply()
+     * @param _kittyId accepts uint for CryptoKitty
+     */
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
+    }
+}
