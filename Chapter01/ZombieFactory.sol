3d1769e03da4f535de2b8b437ebbe34f45bbd317
HardlyCodeMan <72480678+HardlyCodeMan@users.noreply.github.com>
2022-09-29 16:45:41 +0800

    Chapter01

---
+++ Chapter01/ZombieFactory.sol
@@ -0,0 +1,50 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    /// @dev State Variables
+    uint dnaDigits = 16;
+    uint dnaModulus = 10**dnaDigits;
+
+    /// @dev Structs
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    /// @dev Arrays
+    Zombie[] public zombies;
+
+    /// @dev events
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    /// @dev functions
+
+    /***
+     * @param _name accepts string for Zombie name
+     * @param _dna accepts uint for Zombie dna
+     */
+    function _createZombie(string memory _name, uint _dna) private {
+        /// @dev add new zombie to array
+        Zombie _newZombie = Zombie(_name, _dna);
+        uint id = zombies.push(_newZombie) -1;
+        // zombies.push(Zombie(_name, _dna)); // 1 liner variation of array push
+        emit NewZombie(id, _name, _dna);
+    }
+
+    /***
+     * @param _str accepts string for entropy
+     */
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));    /// @audit insecure method of randomness
+        return rand % dnaModulus;
+    }
+
+    /***
+     * @param _name accepts string for Zombie name
+     */
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
