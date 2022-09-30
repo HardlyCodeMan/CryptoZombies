6100d5bc95caa5c5623763ab78302bdfa8e5d768
HardlyCodeMan <72480678+HardlyCodeMan@users.noreply.github.com>
2022-09-30 13:46:13 +0800

    Chapter01 - Lesson03

---
+++ Chapter01/Lesson03/ZombieFactory.sol
@@ -0,0 +1,65 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "OpenZeppelin/Ownable.sol";
+
+contract ZombieFactory is Ownable {
+
+    /// @dev State Variables
+    uint dnaDigits = 16;
+    uint dnaModulus = 10**dnaDigits;
+    uint cooldownTime = 1 days;
+
+    /// @dev Structs
+    struct Zombie {
+        string name;
+        uint dna;
+        uint32 level;
+        uint32 readyTime;
+    }
+
+    /// @dev Arrays
+    Zombie[] public zombies;
+
+    /// @dev Mappings
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    /// @dev events
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    /// @dev functions
+
+    /***
+     * @param _name accepts string for Zombie name
+     * @param _dna accepts uint for Zombie dna
+     */
+    function _createZombie(string memory _name, uint _dna) internal {
+        /// @dev add new zombie to array
+        Zombie _newZombie = Zombie(_name, _dna, 1, uint32(now + cooldownTime));
+        uint id = zombies.push(_newZombie) -1;
+        // zombies.push(Zombie(_name, _dna, 1, now + coolddownTime)); // 1 liner variation of array push
+
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender] ++;
+
+        emit NewZombie(id, _name, _dna);
+    }
+
+    /***
+     * @param _str accepts string for entropy
+     */
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));    /// @audit insecure method of randomness
+        return rand % dnaModulus;
+    }
+
+    /***
+     * @dev only create a zombie if the account hasn't yet created one
+     * @param _name accepts string for Zombie name
+     */
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0, "Error creating zombie, user already created a zombie");
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
---
+++ Chapter01/Lesson03/ZombieHelper.sol
@@ -0,0 +1,52 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./ZombieFeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+    
+    /***
+     * @dev ensure zombie is at or above a certain level
+     * @param _level check value
+     * @param _zombieId zombie to check
+     */
+    modifier aboveLevel(uint _level, uint _zombieId) {
+        require(zombies[_zombieId].level >= _level, "Error zombie level not high enough");
+        _;
+    }
+
+    /***
+     * @dev allow user to change zombie name from level 2
+     * @param _zombieId zombie to change name
+     * @param _newName new name for the zombie
+     */
+    function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+        require(zombieToOwner[_zombieId] == msg.sender);
+        zombies[_zombieId].name = _newName;
+    }
+
+    /***
+     * @dev allow user to change zombie dna from level 20
+     * @param _zombieId zombie to change dna
+     * @param _newDna new dna for the zombie
+     */
+    function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+        require(zombieToOwner[_zombieId] == msg.sender);
+        zombies[_zombieId].dna = _newDna;
+    }
+
+    /***
+     * @dev returm an array of all the zombies owned by the owner
+     * @param _owner owner address to load all the zombies
+     */
+    function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
+        uint[] memory result = new uint[](ownerZombieCount[_owner]);
+        uint counter = 0;
+        for (uint i = 0; i < zombies.length; i++) {
+            if (zombieToOwner[i] == _owner) {
+              result[counter] = i;
+              counter++;
+            }
+        }
+        return result;
+    }
+}
